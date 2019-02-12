d7353a7420eeba1ab90698cb448b1cfb108a35cd
Fillan Lee <fillanlee@aol.com>
2019-02-12 04:08:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,6 @@ def adding_to_dennis
      }
    
    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly" 
-end
-binding.pry
+   binding.pry
  programmer_hash[:dennis_ritchie][:languages][1]
+end
