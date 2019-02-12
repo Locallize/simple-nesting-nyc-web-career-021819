0f262be4aaccfaf089af6d631d7be7e80f1054ab
Fillan Lee <fillanlee@aol.com>
2019-02-12 03:43:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,10 +110,10 @@ def changing_alan
      
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
- 
+ programmer_hash[:alan_kay][:known_for]
      
 end
-programmer_hash[:alan_kay][:known_for]
+changing_alan
 def adding_to_dennis
   
 	programmer_hash = 
